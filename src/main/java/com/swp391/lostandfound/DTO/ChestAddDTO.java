214064640c4b06c07ee4fd79813286eafd224d96
HuyKunNe <dinhquanghuydt@gmail.com>
2022-05-26 02:59:20 +0700

    imp API of chest

---
+++ src/main/java/com/swp391/lostandfound/DTO/ChestAddDTO.java
@@ -0,0 +1,11 @@
+package com.swp391.lostandfound.DTO;
+
+import lombok.Data;
+
+@Data
+public class ChestAddDTO {
+    private String name;
+    private String description;
+    private String location;
+
+}
