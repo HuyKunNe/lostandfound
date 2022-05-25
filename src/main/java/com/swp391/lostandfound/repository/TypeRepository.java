31a58513cea618dd9b2b4e71ad4310f2c205758e
HuyKunNe <dinhquanghuydt@gmail.com>
2022-05-25 16:24:50 +0700

    imp source base

--- src/main/java/com/swp391/lostandfound/repository/TypeRepository.java
+++ src/main/java/com/swp391/lostandfound/repository/TypeRepository.java
@@ -1,10 +1,10 @@
 package com.swp391.lostandfound.repository;
 
+import com.swp391.lostandfound.entity.Type;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import aj.org.objectweb.asm.Type;
 
 @Repository
 @Transactional
