12abe4f79bc1c87fa172a13123418ad4cd0fd150
DESKTOP-QI7UFJE\DataD <dat7124@gmail.com>
2022-06-04 09:58:24 +0700

    Add UserActivityController

---
+++ src/main/java/com/swp391/lostandfound/dataFormat/ListUserActivity.java
@@ -0,0 +1,14 @@
+package com.swp391.lostandfound.dataFormat;
+
+import com.swp391.lostandfound.entity.ChestItem;
+import com.swp391.lostandfound.entity.UserActivity;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ListUserActivity {
+    private String message;
+    private List<UserActivity> data;
+    private String status;
+}
--- src/main/java/com/swp391/lostandfound/repository/UserActivityRepository.java
+++ src/main/java/com/swp391/lostandfound/repository/UserActivityRepository.java
@@ -1,8 +1,11 @@
 package com.swp391.lostandfound.repository;
 
+import com.swp391.lostandfound.entity.ChestItem;
 import com.swp391.lostandfound.entity.UserActivity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserActivityRepository extends JpaRepository<UserActivity, Integer> {
 
