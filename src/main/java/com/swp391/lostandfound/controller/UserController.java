7ad008d1bbee1d6b0399dcdbf966a932042eabc3
nhd1123 <parkunduong@gmail.com>
2022-07-11 16:55:59 +0700

    fix

--- src/main/java/com/swp391/lostandfound/controller/UserController.java
+++ src/main/java/com/swp391/lostandfound/controller/UserController.java
@@ -7,7 +7,6 @@ import com.swp391.lostandfound.entity.User;
 import com.swp391.lostandfound.jwt.JwtConfig;
 import com.swp391.lostandfound.service.UserService;
 
-<<<<<<< HEAD
 import io.jsonwebtoken.Jwts;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +22,6 @@ import javax.crypto.SecretKey;
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.Date;
-=======
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +30,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> 75eb0d220f03f0b1dadd7a798cb43e46f230d559
 
 @RestController
 @RequestMapping
@@ -134,7 +131,6 @@ public class UserController {
     }
 
     @PostMapping("/user")
-<<<<<<< HEAD
     public ResponseEntity<ResponseDTO> createUser(UserAddDTO userAddDTO) {
         ResponseDTO responseDTO = new ResponseDTO();
         this.userService.addUser(UserAddDTO.builder().firstName(userAddDTO.getFirstName()).lastName(userAddDTO.getLastName())
@@ -144,52 +140,29 @@ public class UserController {
         Authentication authentication = new UsernamePasswordAuthenticationToken(userAddDTO.getStudentCode(), userAddDTO.getPassword());
         try {
             Authentication authenticate = authenticationManager.authenticate(authentication);
-        if(authenticate.isAuthenticated()){
-        User userAuthenticated = userService.findUserByStudentCode(userAddDTO.getStudentCode());
+            if (authenticate.isAuthenticated()) {
+                User userAuthenticated = userService.findUserByStudentCode(userAddDTO.getStudentCode());
 
-        String token = Jwts.builder().setSubject(authenticate.getName())
-                .claim("authorities", authenticate.getAuthorities()).claim("userId", userAuthenticated.getId())
-                .setIssuedAt((new Date())).setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(1)))
-                .signWith(secretKey).compact();
+                String token = Jwts.builder().setSubject(authenticate.getName())
+                        .claim("authorities", authenticate.getAuthorities()).claim("userId", userAuthenticated.getId())
+                        .setIssuedAt((new Date())).setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(1)))
+                        .signWith(secretKey).compact();
 
-        LoginResponseDTO loginResponseDTO = LoginResponseDTO.builder().firstName(userAuthenticated.getFirstName())
-                .lastName(userAuthenticated.getLastName()).studentCode(userAuthenticated.getStudentCode())
-                .email(userAuthenticated.getEmail()).role(userAuthenticated.getRole()).token(jwtConfig.getTokenPrefix() + token).build();
+                LoginResponseDTO loginResponseDTO = LoginResponseDTO.builder().firstName(userAuthenticated.getFirstName())
+                        .lastName(userAuthenticated.getLastName()).studentCode(userAuthenticated.getStudentCode())
+                        .email(userAuthenticated.getEmail()).role(userAuthenticated.getRole()).token(jwtConfig.getTokenPrefix() + token).build();
 
-        responseDTO.setData(loginResponseDTO);
-        responseDTO.setSuccessCode("Create User Succcessfully");
-        return ResponseEntity.ok().body(responseDTO);
-        }else {
-            responseDTO.setErrorCode("Create User Failed");
-            return ResponseEntity.badRequest().body(responseDTO);
-=======
-    public UserData createUser(UserAddDTO userAddDTO) {
-        UserData userData = new UserData();
-        User user = userService.addUser(userAddDTO);
-        if (user != null) {
-            userData.setMessage("Create User Successfully");
-            userData.setUser(user);
-            userData.setStatus("Success");
-        } else {
-            userData.setMessage("Create User Failed");
-            userData.setStatus("Fail");
->>>>>>> 75eb0d220f03f0b1dadd7a798cb43e46f230d559
-        }
-        }catch (Exception e){
-                responseDTO.setErrorCode(e.getMessage());
+                responseDTO.setData(loginResponseDTO);
+                responseDTO.setSuccessCode("Create User Succcessfully");
+                return ResponseEntity.ok().body(responseDTO);
+            } else {
+                responseDTO.setErrorCode("Create User Failed");
                 return ResponseEntity.badRequest().body(responseDTO);
+            }
+        } catch (Exception e){
+            responseDTO.setErrorCode(e.getMessage());
+            return  ResponseEntity.badRequest().body(responseDTO);
         }
-//        User user = userService.addUser(userAddDTO);
-//        if (user != null) {
-//            userData.setMessage("Create User Succcessfully");
-//            userData.setUser(user);
-//            userData.setStatus("Success");
-//        } else {
-//            userData.setMessage("Create User Failed");
-//            userData.setStatus("Fail");
-//        }
-//
-//        return userData;
     }
 
     @DeleteMapping("/user/{id}")
