5cde66b11746c69f9009e489fd9c03f24f9c1db7
Huy Kun <dinhquanghuydt@gmail.com>
2022-07-12 20:23:02 +0700

    cros

--- src/main/java/com/swp391/lostandfound/LostandfoundApplication.java
+++ src/main/java/com/swp391/lostandfound/LostandfoundApplication.java
@@ -2,9 +2,6 @@ package com.swp391.lostandfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LostandfoundApplication {
@@ -13,14 +10,6 @@ public class LostandfoundApplication {
 		SpringApplication.run(LostandfoundApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+	
 
 }
