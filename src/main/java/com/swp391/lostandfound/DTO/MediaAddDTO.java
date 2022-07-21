d4bff5ee0fd53403679ef416204cbb65e6c2b31d
Huy Kun <dinhquanghuydt@gmail.com>
2022-07-21 15:01:17 +0700

    revert code

--- src/main/java/com/swp391/lostandfound/DTO/MediaAddDTO.java
+++ src/main/java/com/swp391/lostandfound/DTO/MediaAddDTO.java
@@ -1,12 +1,10 @@
 package com.swp391.lostandfound.DTO;
 
-import java.util.List;
-
 import lombok.Data;
 
 @Data
 public class MediaAddDTO {
 
-    private List<String> link;
+    private String link;
     private int postId;
 }
--- src/main/java/com/swp391/lostandfound/DTO/MediaUpdateDTO.java
+++ src/main/java/com/swp391/lostandfound/DTO/MediaUpdateDTO.java
@@ -1,12 +1,10 @@
 package com.swp391.lostandfound.DTO;
 
-import java.util.List;
-
 import lombok.Data;
 
 @Data
 public class MediaUpdateDTO {
-    private List<String> link;
+    private String link;
     private int status;
     private int postId;
 }
